d5d57ba61b11ba8b78f79b18833f60255e51b5a6
vinghtcinqmay <vingtcinqmay@gmail.com>
2026-07-27 11:28:16 -0400

    fxed dt code

--- TeamCode/src/main/java/teleop/Hardware.java
+++ TeamCode/src/main/java/teleop/Hardware.java
@@ -7,6 +7,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.opencv.core.Mat;
+
 import java.util.concurrent.TimeUnit;
 
 public class Hardware {
@@ -55,16 +57,31 @@ public class Hardware {
             singleMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
 
+        /**
+        double maxPower = 1.0;
+        /**
+        maxPower = Math.max(maxPower, Math.abs(forward + strafe + rotate));
+        maxPower = Math.max(maxPower, Math.abs(forward - strafe + rotate));
+        maxPower = Math.max(maxPower, Math.abs(forward - strafe - rotate));
+        maxPower = Math.max(maxPower, Math.abs(forward + strafe - rotate));
+        **/
         double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
+        /**
+        double frontLeftPower = (forward + strafe + rotate) / maxPower;
+        double backLeftPower = (forward - strafe + rotate) / maxPower;
+        double frontRightPower = (forward - strafe - rotate) / maxPower;
+        double backRightPower = (forward + strafe - rotate) / maxPower;
+         **/
+        double frontLeftPower = (forward + strafe + rotate) / denominator;
+        double backLeftPower = (forward - strafe + rotate) / denominator;
+        double frontRightPower = (forward - strafe - rotate) / denominator;
+        double backRightPower = (forward + strafe - rotate) / denominator;
 
-        double frontLeftPower = (forward + strafe + rotate)/denominator;
-        double backLeftPower = (forward - strafe + rotate)/denominator;
-        double frontRightPower = (forward - strafe - rotate)/denominator;
-        double backRightPower = (forward + strafe - rotate)/denominator;
+
 
         frontLeftMotor.setPower(frontLeftPower);
-        frontRightMotor.setPower(backLeftPower);
-        backLeftMotor.setPower(frontRightPower);
+        frontRightMotor.setPower(frontRightPower);
+        backLeftMotor.setPower(backLeftPower);
         backRightMotor.setPower(backRightPower);
 
     }
@@ -111,8 +128,9 @@ public class Hardware {
         Methods.SLEEP(5000); //Wait for it to finish
         slides.setTargetPosition(0);
         drill.setPower(0);
-        slides.setPower(1); // Go back to 0
+        slides.setPower(0.8); // Go back to 0
         Methods.SLEEP(1000);
+        slides.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slides.setPower(0.0); //Hold Position Up
         state[0]=Constants.TeleOpState.FREE;
     }
@@ -123,6 +141,7 @@ public class Hardware {
         state[0] = Constants.TeleOpState.ACTION_OCCUPIED;
         drillSequence(state);
         moveToPlant(state);
+        Methods.SLEEP(100);
         dispenseSeed(state);
         state[0] = Constants.TeleOpState.FREE;
     }
