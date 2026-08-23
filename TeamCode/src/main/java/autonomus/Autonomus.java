5b09f2c65aa7237697dfafe8fa33a0f01c7f3d49
huange384 <huange384@gmail.com>
2026-08-22 21:22:58 -0400

    Create Autonomus.java

---
+++ TeamCode/src/main/java/autonomus/Autonomus.java
@@ -0,0 +1,18 @@
+package autonomus;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous (name = "SeedbotAuto")
+public class Autonomus extends OpMode {
+    @Override
+    public void init()
+    {
+
+    }
+    @Override
+    public void loop()
+    {
+
+    }
+}
